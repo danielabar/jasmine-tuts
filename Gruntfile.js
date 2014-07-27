5f44056090230a84dea8e41d15be2ab2241a460c
Daniela Baron <baron.daniela@gmail.com>
2014-07-27 19:47:54 -0400

    use bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,7 @@ module.exports = function(grunt) {
       test : {
         files : [
           'Gruntfile.js',
+          'SpecRunner.html',
           'src/**/*.js',
           'spec/**/*.js'
         ],
