269e59b1135b7ad8a667d8d633913d02cb51da3f
Daniela Baron <baron.daniela@gmail.com>
2014-07-27 18:01:37 -0400

    add grunt task to run jasmine tests

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    jasmine : {
+      src : 'src/**/*.js',
+      options : {
+        specs : 'spec/**/*.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.registerTask('test', ['jasmine']);
+
+  grunt.registerTask('default', ['test']);
+
+};
