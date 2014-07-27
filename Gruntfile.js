f1ff979c5e4674a97274640921dd559c9664814d
Daniela Baron <baron.daniela@gmail.com>
2014-07-27 18:55:38 -0400

    grunt tasks to re-run jasmine tests on connect server and watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,47 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+
+    // Start a server for running tests
+    connect : {
+      test : {
+        options : {
+          port: 9000,
+          livereload: true
+        }
+      }
+    },
+
+    // Re-run tests on any change to source or spec files
+    watch : {
+      test : {
+        files : [
+          'Gruntfile.js',
+          'src/**/*.js',
+          'spec/**/*.js'
+        ],
+        options: {
+          atBegin: true,
+        },
+        tasks: ['jasmine']
+      }
+    },
+
+    // Run Jasmine tests on the connect server
     jasmine : {
       src : 'src/**/*.js',
       options : {
-        specs : 'spec/**/*.js'
+        version: '2.0.1',
+        specs: 'spec/**/*.js',
+        host: 'http://0.0.0.0:9000'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('test', ['jasmine']);
-
-  grunt.registerTask('default', ['test']);
+  grunt.registerTask('default', ['connect:test', 'watch:test']);
 
 };
