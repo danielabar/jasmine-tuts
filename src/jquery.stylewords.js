8b4b3a39dbe31faebf5e4627cf27b2809dc50acb
Daniela Baron <baron.daniela@gmail.com>
2014-08-01 20:44:09 -0400

    refactor plugin

--- src/jquery.stylewords.js
+++ src/jquery.stylewords.js
@@ -1,15 +1,13 @@
 ! function($) {
   $.fn.stylewords = function() {
-
     return this.each(function() {
-      var words = $(this).text().split(' ');
-      var wrapper = $('<span></span>').text(words.shift());
-      wrapper = $('<div>').append(wrapper).html();
-      $(this).html(function() {
+      var el = $(this);
+      el.html(function() {
+        var words = el.text().split(' ');
+        var wrapper = $('<span></span>').text(words.shift());
+        wrapper = $('<div>').append(wrapper).html();
         return [wrapper].concat(words).join(' ');
       });
     });
-
   };
-
 }(jQuery);
