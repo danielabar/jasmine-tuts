5d6afde20fd008c0d894d3cd4bd2a336904d89d0
Daniela Baron <baron.daniela@gmail.com>
2014-08-02 17:59:22 -0400

    finish refactor plugin

--- src/jquery.stylewords.js
+++ src/jquery.stylewords.js
@@ -1,28 +1,42 @@
-! function($) {
+(function($) {
+  var stylewords = {
+
+    init: function(el, numwords, config) {
+      this.numwords = numwords || 1;
+      this.config = $.extend({}, $.fn.stylewords.defaults, config);
+      this.el = el;
+      this.$el = $(el);
+      this.updateHTML();
+    },
+
+    updateHTML: function() {
+      var self = this;
+      this.$el.html(function(i, text) {
+        return self.createWrapper(text);
+      });
+    },
+
+    createWrapper: function(text) {
+      var words = text.split(' ');
+      var tag = this.config.tag;
+      var wrapOpts = {
+        'class': this.config['class']
+      };
+      var wrapper = $(this.config.tag, wrapOpts).text(words.splice(0, this.numwords).join(' '));
+      wrapper = $('<div>').append(wrapper).html();
+      return [wrapper].concat(words).join(' ');
+    }
+  };
 
   $.fn.stylewords = function(numwords, config) {
-    numwords = numwords || 1;
-    config = $.extend({}, $.fn.stylewords.defaults, config);
-
+    var obj = Object.create(stylewords);
     return this.each(function() {
-      var el = $(this);
-      el.html(function() {
-        var words = el.text().split(' ');
-        var wrapOpts = {
-          'class': config['class']
-        };
-
-        var wrapper = $(config.tag, wrapOpts).text(words.splice(0, numwords).join(' '));
-        wrapper = $('<div>').append(wrapper).html();
-        return [wrapper].concat(words).join(' ');
-      });
+      obj.init(this, numwords, config);
     });
-
   };
 
   $.fn.stylewords.defaults = {
     tag: '<span>',
     'class': 'stylewords'
-  }
-
-}(jQuery);
+  };
+})(jQuery);
