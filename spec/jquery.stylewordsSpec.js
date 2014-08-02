17333cf6f26abe595cfa8adea299ff2abd7ad721
Daniela Baron <baron.daniela@gmail.com>
2014-08-02 10:36:11 -0400

    more plugin testing to verify config and overrides

--- spec/jquery.stylewordsSpec.js
+++ spec/jquery.stylewordsSpec.js
@@ -20,4 +20,28 @@ describe("jQuery.stylewords", function() {
     expect(frag.find('span').length).toEqual(1);
   });
 
+  it("Defaults to wrapping only one word", function() {
+    frag.stylewords();
+    expect(frag.find('span').text()).toEqual('Lorem');
+  });
+
+  it("Allows user to specify number of words to be wrapped", function() {
+    frag.stylewords(3);
+    expect(frag.find('span').text()).toEqual('Lorem ipsum dolor');
+  });
+
+  it("Offers a defaults object on the styleords namespace", function() {
+    expect($.fn.stylewords.defaults).toBeDefined();
+  });
+
+  it("Allows the user to override the defaults", function() {
+    frag.stylewords(2, {
+      id: 'some-id',
+      'class': 'some-class',
+      tag: '<strong>'
+    });
+    expect(frag.find('strong').length).toEqual(1);
+    expect(frag.find('strong').text()).toEqual('Lorem ipsum');
+  });
+
 });
--- src/jquery.stylewords.js
+++ src/jquery.stylewords.js
@@ -1,13 +1,24 @@
 ! function($) {
-  $.fn.stylewords = function() {
+
+  $.fn.stylewords = function(numwords, config) {
+    numwords = numwords || 1;
+    config = $.extend({}, $.fn.stylewords.defaults, config);
+
     return this.each(function() {
       var el = $(this);
       el.html(function() {
         var words = el.text().split(' ');
-        var wrapper = $('<span></span>').text(words.shift());
+        var wrapper = $(config.tag).text(words.splice(0, numwords).join(' '));
         wrapper = $('<div>').append(wrapper).html();
         return [wrapper].concat(words).join(' ');
       });
     });
+
   };
+
+  $.fn.stylewords.defaults = {
+    tag: '<span>',
+    'class': 'stylewords'
+  }
+
 }(jQuery);
